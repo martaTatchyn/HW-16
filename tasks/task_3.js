e531453f6b47d00202305582ac72accf7e306abe
Marta <marta.tatchyn@gmail.com>
2026-02-08 12:39:36 +0000

    first commit

---
+++ tasks/task_3.js
@@ -0,0 +1,15 @@
+const storage = new Storage([
+  "Нанітоіди",
+  "Пролонгер",
+  "Залізні жупи",
+  "Антигравітатор",
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem("Дроїд");
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
